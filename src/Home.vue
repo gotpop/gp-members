c10f93fe2563d308d109cb66f26718338c111a45
gotpop <liamsobczak@gmail.com>
2017-06-19 21:27:57 +0100

    'Setup'

--- src/Home.vue
+++ src/Home.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <h1>Welcome</h1>
-
     <app-input></app-input>
   </div>
 </template>
@@ -18,32 +16,8 @@ import Input from './Input.vue'
 
 <style lang="scss">
 
-// @import '~bulma';
+@import '~bulma';
 
 
-#app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
 
-h1,
-h2 {
-    font-weight: normal;
-}
-
-ul {
-    list-style-type: none;
-    padding: 0;
-}
-
-li {
-    display: inline-block;
-    margin: 0 10px;
-}
-
-a {
-    color: #42b983;
-}
 </style>
--- src/Input.vue
+++ src/Input.vue
@@ -1,7 +1,64 @@
 <template>
 <div>
-  <input type="text" v-model="message">
-  <p>{{message}}</p>
+
+
+
+  <nav class="nav has-shadow">
+    <div class="container">
+      <div class="nav-left">
+        <a class="nav-item">
+          <img src="http://bulma.io/images/bulma-logo.png" alt="Bulma logo">
+        </a>
+        <a class="nav-item is-tab is-hidden-mobile is-active">Home</a>
+        <a class="nav-item is-tab is-hidden-mobile">Features</a>
+        <a class="nav-item is-tab is-hidden-mobile">Pricing</a>
+        <a class="nav-item is-tab is-hidden-mobile">About</a>
+      </div>
+      <span class="nav-toggle">
+        <span></span>
+        <span></span>
+        <span></span>
+      </span>
+      <div class="nav-right nav-menu">
+        <a class="nav-item is-tab is-hidden-tablet is-active">Home</a>
+        <a class="nav-item is-tab is-hidden-tablet">Features</a>
+        <a class="nav-item is-tab is-hidden-tablet">Pricing</a>
+        <a class="nav-item is-tab is-hidden-tablet">About</a>
+        <a class="nav-item is-tab">
+          <figure class="image is-16x16" style="margin-right: 8px;">
+            <img src="http://bulma.io/images/jgthms.png">
+          </figure>
+          Profile
+        </a>
+        <a class="nav-item is-tab">Log out</a>
+      </div>
+    </div>
+  </nav>
+
+
+
+
+
+
+  <div class="container">
+
+
+    <div class="columns">
+      <div class="column">
+
+
+
+        <input type="text" v-model="message">
+        <p>{{message}}</p>
+      </div>
+    </div>
+
+
+  </div>
+
+
+  <div class="container">
+
   <div class="columns">
         <div class="column">
           <p class="notification is-info">First column</p>
@@ -17,6 +74,18 @@
         </div>
     </div>
 </div>
+
+
+
+
+
+
+
+</div>
+
+
+
+
 </template>
 
 
