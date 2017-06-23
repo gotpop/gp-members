a5bfc5fee7dd60d802ea92cdd804db2998c2bec4
gotpop <liamsobczak@gmail.com>
2017-06-23 18:04:48 +0100

    'Stores'

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,49 @@
 <template>
 <div class="wrap-site">
   <gp-header></gp-header>
-  <router-view></router-view>
+  <transition name="fade">
+   <router-view></router-view>
+ </transition>
 </div>
 </template>
 
 <style lang="scss">
 @import '~bulma';
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .6s
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */ {
+  opacity: 0
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition-property: opacity;
+  transition-duration: .25s;
+}
+
+.fade-enter-active {
+  transition-delay: .25s;
+}
+
+.fade-enter, .fade-leave-active {
+  opacity: 0
+}
+
+
+/* Enter and leave animations can use different */
+/* durations and timing functions.              */
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active for <2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
 
 .nav-item a.is-tab.router-link-exact-active,
 a.nav-item.is-tab.router-link-exact-active {
--- src/Summary.vue
+++ src/Summary.vue
@@ -4,9 +4,11 @@
 
     <div class="columns">
       <div class="column">
-        <div class="notification is-success" v-show="showNote">
-          <button class="delete" v-on:click="showNote = false"></button> Form data has been to submitted to the server via an Express endpoint, and then returned to the summary page in the table below.
-        </div>
+        <transition name="fade">
+          <div class="notification is-success" v-show="showNote">
+            <button class="delete" v-on:click="showNote = false"></button> Form data has been to submitted to the server via an Express endpoint, and then returned to the summary page in the table below.
+          </div>
+        </transition>
       </div>
     </div>
   </div>
@@ -24,25 +26,32 @@
           </thead>
           <tbody>
             <tr>
-              <td>Name</td><td>{{serverUserObject.name}}</td>
+              <td>Name</td>
+              <td>{{serverUserObject.name}}</td>
             </tr>
             <tr>
-              <td>Surname</td><td>{{serverUserObject.surname}}</td>
+              <td>Surname</td>
+              <td>{{serverUserObject.surname}}</td>
             </tr>
             <tr>
-              <td>Email</td><td>{{serverUserObject.email}}</td>
+              <td>Email</td>
+              <td>{{serverUserObject.email}}</td>
             </tr>
             <tr>
-              <td>Address1</td><td>{{serverUserObject.address1}}</td>
+              <td>Address1</td>
+              <td>{{serverUserObject.address1}}</td>
             </tr>
             <tr>
-              <td>Address1</td><td>{{serverUserObject.address2}}</td>
+              <td>Address1</td>
+              <td>{{serverUserObject.address2}}</td>
             </tr>
             <tr>
-              <td>Postcode</td><td>{{serverUserObject.postcode}}</td>
+              <td>Postcode</td>
+              <td>{{serverUserObject.postcode}}</td>
             </tr>
             <tr>
-              <td>List</td><td>{{serverUserObject.list}}</td>
+              <td>List</td>
+              <td>{{serverUserObject.list}}</td>
             </tr>
           </tbody>
         </table>
